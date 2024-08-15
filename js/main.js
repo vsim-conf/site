49de7a465ba1abb8830040a50f548e968c642bd4
angel-marchev <angel.marchev@yahoo.com>
2024-08-15 13:28:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function($) {
 	//COUNTDOWN TIMER
 	var newYear = new Date(); 
     newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
-    $('#countdown').countdown({until: new Date(2024, 9-1, 10, 9, 0, 0)}); // enter event day
+    $('#countdown').countdown({until: new Date(2024, 9-1, 11, 9, 0, 0)}); // enter event day
     
     $('#removeCountdown').toggle(
         function() {
